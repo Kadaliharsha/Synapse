301f8cb65750f4826f67658bf2b9dd4f4cddc3d9
Kadaliharsha <kadali.hrv@gmail.com>
2026-01-10 13:16:06 +0530

    allow dynamic api url

--- Synapse/frontend/src/api/api.js
+++ Synapse/frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: import.meta.env.VITE_API_URL || "http://localhost:8080",
 });
 
 // Add a request interceptor to attach the JWT token
